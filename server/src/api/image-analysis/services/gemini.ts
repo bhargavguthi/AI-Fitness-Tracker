1320f42985f51a0a8133e851c2cd2912ed939075
Guthi Srinivasa Bhargav <srinivasabhargavguthi@gmail.com>
2026-07-26 19:50:32 +0530

    Fix Gemini model

--- server/src/api/image-analysis/services/gemini.ts
+++ server/src/api/image-analysis/services/gemini.ts
@@ -33,7 +33,7 @@ export const analyzeImage = async (filePath: string)=>{
     }
 
     const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
+    model: "gemini-2.0-flash",
     contents: contents,
     config
     });
